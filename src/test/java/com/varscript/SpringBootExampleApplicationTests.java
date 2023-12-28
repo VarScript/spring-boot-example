625c7b034c93439ea42df8eb5935253fb3e51b8c
varscript <vargassanchezfabio01@gmail.com>
2023-12-28 18:24:24 -0500

    Initial commit

---
+++ src/test/java/com/varscript/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
